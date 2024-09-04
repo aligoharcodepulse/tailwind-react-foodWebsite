5343519c3967173a9f5932ef4d0341d49ddfbe80
Ali Gohar <aligoharkp@gmail.com>
2024-09-04 16:42:06 +0500

    done with hero section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,6 @@ const Navbar = () => {
     <div className="flex justify-between px-5 p-2">
     <div className="flex justify-center items-center">
       <img src={Img} className="w-36 h-20"/>
-      <h2 className="font-bold text-gray-600 text-2xl ml-3">Foodpanda</h2>
     </div>
     <div className="flex gap-x-7 justify-center items-center">
         <h2 className="text-2xl text-gray-600 hover:text-red-600 font-semibold hover:cursor-pointer">Home</h2>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,18 @@
-
-
+//import banner  from '../images/banner.jpg'
+import '../App.css'
 const Home = () => {
   return (
     <div>
-      Hero Section
+      {/* Hero Section */}
+      <section id="heroSection" className='flex justify-center items-center'>
+        <input type="text" placeholder='Search food items' className='w-[50vw] px-4 py-2 focus:outline-none border border-gray-500 rounded-md'/>
+        <button className='bg-gradient-to-tl from-green-500 to-cyan-500 text-white py-2 px-4 ml-2 border border-gray-500 rounded-md'>Search</button>
+      </section>
+
+      {/* Explore Foods */}
+      <section>
+        <h2 className='text-3xl font-bold text-gray-700 mt-6 flex justify-center'>Explore Foods</h2>
+      </section>
     </div>
   )
 }
